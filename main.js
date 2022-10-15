1f46b03b6b71bdb3821f59304fc5433688658d62
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-10-14 20:50:43 -0500

    Conexion a los datos del PHP

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-addEventListener("DOMContentLoaded", (e) => {
+addEventListener("DOMContentLoaded", async(e) => {
+    let peticion = await fetch("https://comfenalcocursos.000webhostapp.com/Actividad_maquetacion_web_12_10_2022/api.php");
+    let datos = await peticion.json();
+    console.log(datos);
+    document.querySelector(".logo").innerHTML = datos.imgenEmpresa;
+    document.querySelector(".navigation li:nth-child(1) a").innerHTML = datos.menu[0];
+    document.querySelector(".navigation li:nth-child(2) a").innerHTML = datos.menu[1];
+    document.querySelector(".navigation li:nth-child(3) a").innerHTML = datos.menu[2];
+    document.querySelector(".navigation li:nth-child(4) a").innerHTML = datos.menu[3];
+    document.querySelector("#text").innerHTML = datos.titulo;
+    document.querySelector(".sec h2").innerHTML = datos.secion;
+    document.querySelector(".sec p").innerHTML = `Nombre del servidor PHP: ${datos.servidor} <br><br> ${datos.parrafo}`;
+
     gsap.from("#m1", {
         scrollTrigger: {
             scrub: true
@@ -41,4 +53,4 @@ addEventListener("DOMContentLoaded", (e) => {
         },
         x: 600,
     })
-})
+}) 
